952eb957292316bc9fa70397cfbb9e28b618827e
aaschwab <aries7020@gmail.com>
2024-03-08 17:26:28 -0600

    inserted apiUrl & Key created functions
    inserted apiKey & apiUrl
    created function to pull city from searchInput.value
    created a response function to get a response from Api on temperature

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "44ebfbe3f90e8afo31a00943c4996bft";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=imperial`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#add-your-city");
