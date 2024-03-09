20ce814d9b680f6445f5b1d7b6cb80a05f343b89
aaschwab <aries7020@gmail.com>
2024-03-08 18:39:38 -0600

    injected more weather data

--- index.js
+++ index.js
@@ -5,12 +5,35 @@ function refreshWeather(response) {
   let discriptionElement = document.querySelector("#weather-discription");
   let humidityElement = document.querySelector("#humidity-percentage");
   let windElement = document.querySelector("#wind-mph");
+  let timeElement = document.querySelector("#current-date");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
   discriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}mph`;
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
